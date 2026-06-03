dd4459df0df706d0490687639ef15d9aa4706031
Mohit Singh Negi <mnegi7668@gmail.com>
2026-06-03 23:47:42 +0530

    Billing Service Added

---
+++ billing-service/src/main/java/com/patientManagementSystem/billingService/grpc/BillingGrpcService.java
@@ -0,0 +1,9 @@
+package com.patientManagementSystem.billingService.grpc;
+
+import net.devh.boot.grpc.server.service.GrpcService;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceImplBase {
+
+}
