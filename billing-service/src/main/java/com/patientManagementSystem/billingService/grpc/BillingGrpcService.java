9908b40ee19172037e50af53b38137da48141185
Mohit Singh Negi <mnegi7668@gmail.com>
2026-06-05 00:05:59 +0530

    GRPC Implementation

--- billing-service/src/main/java/com/patientManagementSystem/billingService/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/patientManagementSystem/billingService/grpc/BillingGrpcService.java
@@ -1,9 +1,27 @@
 package com.patientManagementSystem.billingService.grpc;
 
+import billing.BillingResponse;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 import billing.BillingServiceGrpc.BillingServiceImplBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+@Slf4j
 @GrpcService
 public class BillingGrpcService extends BillingServiceImplBase {
 
+    @Override
+    public void createBillingAccount(billing.BillingRequest billingRequest, StreamObserver<billing.BillingResponse> responseStreamObserver){
+        log.info("CreateBillingAccount request received {}",billingRequest.toString());
+
+        BillingResponse response = BillingResponse.newBuilder()
+                .setAccountId("12345")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseStreamObserver.onNext(response);
+        responseStreamObserver.onCompleted();
+    }
 }
